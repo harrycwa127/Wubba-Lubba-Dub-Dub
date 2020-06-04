444f843bb0f00d2afe4a06a03f394f5cf088ce73
harrycwa127 <66370343+harrycwa127@users.noreply.github.com>
2020-06-04 22:31:32 +0800

    update

--- bot.py
+++ bot.py
@@ -1,26 +1,49 @@
 import discord
 from discord.ext import commands
+import json
+import random
 
-bot = commands.Bot(command_prefix = "-")
+with open("setting.json", mode="r", encoding="utf8") as jfile:
+    jdata = json.load(jfile)
+
+bot = commands.Bot(command_prefix="-")
+
 
 @bot.event
 async def on_ready():
-    print(">> Bot is online <<")
+    channel = bot.get_channel(int(jdata["channel"]))
+    await channel.send(">> Wubba Lubba Dub-Dub is online <<")
+    print(">> Wubba Lubba Dub-Dub is online <<")
+
 
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(718001395751583764)
+    channel = bot.get_channel(int(jdata["channel"]))
     await channel.send(f"{member} join!")
     print(f"{member} join!")
 
+
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(718001395751583764)
+    channel = bot.get_channel(int(jdata["channel"]))
     await channel.send(f"{member} leave!")
     print(f"{member} leave!")
 
+
 @bot.command()
 async def ping(ctx):
-    await ctx.send(bot.latency)
+    await ctx.send(f"{round(bot.latency*1000)}ms")
 
-bot.run("NzE3Nzk5OTc3NjQwMDAxNTg2.XtiCEw.41PqWkJ93DiwAcrguwug7FwpR6A")
+
+@bot.command()
+async def icon(ctx):
+    pic = discord.File(jdata["icon"])
+    await ctx.send(file=pic)
+
+
+@bot.command()
+async def lick(ctx):
+    random_web = random.choice(jdata["lick"])
+    await ctx.send(random_web)
+
+bot.run(jdata["TOKEN"])
