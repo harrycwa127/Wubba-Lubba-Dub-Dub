15ef0dc57315f0cfb0b4e65553d432bf1a221c55
harrycwa127 <66370343+harrycwa127@users.noreply.github.com>
2020-06-04 16:53:54 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,4 +7,20 @@ bot = commands.Bot(command_prefix = "/")
 async def on_ready():
     print(">> Bot is online <<")
 
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(718001395751583764)
+    await channel.send(f"{member} join!")
+    print(f"{member} join!")
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(718001395751583764)
+    await channel.send(f"{member} leave!")
+    print(f"{member} leave!")
+
+@bot.command
+async def ping(ctx):
+    await ctx.send(bot.latency)
+
 bot.run("NzE3Nzk5OTc3NjQwMDAxNTg2.XtiCEw.41PqWkJ93DiwAcrguwug7FwpR6A")
