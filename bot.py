96e242c55ea3c74683947f080c53a46132db79a5
harrycwa127 <66370343+harrycwa127@users.noreply.github.com>
2020-06-04 16:58:33 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix = "/")
+bot = commands.Bot(command_prefix = "-")
 
 @bot.event
 async def on_ready():
@@ -19,7 +19,7 @@ async def on_member_remove(member):
     await channel.send(f"{member} leave!")
     print(f"{member} leave!")
 
-@bot.command
+@bot.command()
 async def ping(ctx):
     await ctx.send(bot.latency)
 
