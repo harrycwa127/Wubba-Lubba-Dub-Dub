6eb9f6c69b38ce01d11eff2079ce75d1403f9c4d
harrycwa127 <66370343+harrycwa127@users.noreply.github.com>
2020-06-07 19:43:10 +0800

    testing

--- cmds/task.py
+++ cmds/task.py
@@ -5,9 +5,23 @@ import json
 import asyncio
 import datetime
 
+with open("setting.json", mode="r", encoding="utf8") as jfile:
+    jdata = json.load(jfile)
+
 class Task(Cog_Extension):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        async def interval():
+            await self.bot.wait_unit_ready()
+            self.channel = self.bot.get_channel(int(jdata["channel"]))
+            while not self.bot.is_closed():
+                await self.channel.send("I am still alive!")
+                print("loop: I am still alive")
+                await asyncio.sleep(10) #sec
+        
+        self.bg_task = self.bot.loop.create_task(interval())
+
 
 def setup(bot):
     bot.add_cog(Task(bot))
