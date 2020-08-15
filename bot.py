21f4a7711e4bb40dc4aa063d3ce986dea2b5d0cf
harrycwa127 <66370343+harrycwa127@users.noreply.github.com>
2020-08-15 13:34:54 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,43 +4,43 @@ import json
 import os
 import datetime
 
-while True:
-    with open("setting.json", mode="r", encoding="utf8") as jfile:
-        jdata = json.load(jfile)
 
-    bot = commands.Bot(command_prefix=".", description="Harry's Bot")
+with open("setting.json", mode="r", encoding="utf8") as jfile:
+    jdata = json.load(jfile)
 
-
-    @bot.event
-    async def on_ready():
-        print(f"{datetime.datetime.now()} >> Wubba Lubba Dub-Dub is online <<")
+bot = commands.Bot(command_prefix=".", description="Harry's Bot")
 
 
-    @bot.command()
-    async def load(ctx, extension):
-        bot.load_extension(f"cmds.{extension}")
-        await ctx.send(f"Loaded {extension} done.")
-        print(f"{datetime.datetime.now()} Loaded {extension} done.")
+@bot.event
+async def on_ready():
+    print(f"{datetime.datetime.now()} >> Wubba Lubba Dub-Dub is online <<")
 
 
-    @bot.command()
-    async def unload(ctx, extension):
-        bot.unload_extension(f"cmds.{extension}")
-        await ctx.send(f"Un-Loaded {extension} done.")
-        print(f"{datetime.datetime.now()} Un-Loaded {extension} done.")
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f"cmds.{extension}")
+    await ctx.send(f"Loaded {extension} done.")
+    print(f"{datetime.datetime.now()} Loaded {extension} done.")
 
 
-    @bot.command()
-    async def reload(ctx, extension):
-        bot.reload_extension(f"cmds.{extension}")
-        await ctx.send(f"Re-Loaded {extension} done.")
-        print(f"{datetime.datetime.now()} Re-Loaded {extension} done.")
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f"cmds.{extension}")
+    await ctx.send(f"Un-Loaded {extension} done.")
+    print(f"{datetime.datetime.now()} Un-Loaded {extension} done.")
 
 
-    for filename in os.listdir("./cmds"):
-        if filename.endswith(".py"):
-            bot.load_extension(f"cmds.{filename[:-3]}")
+@bot.command()
+async def reload(ctx, extension):
+    bot.reload_extension(f"cmds.{extension}")
+    await ctx.send(f"Re-Loaded {extension} done.")
+    print(f"{datetime.datetime.now()} Re-Loaded {extension} done.")
 
 
-    if __name__ == "__main__":
-        bot.run(jdata["TOKEN"])
+for filename in os.listdir("./cmds"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"cmds.{filename[:-3]}")
+
+
+if __name__ == "__main__":
+    bot.run(jdata["TOKEN"])
